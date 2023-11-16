a3cfd39b1ea1ae1e1b27ace19491f0b162c89221
Christian Hey <c.hey@sbs-herzogenaurach.de>
2023-11-16 12:29:24 +0100

    Solved seconde task.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+# Durch die Funktion place_item werden Packungen auf den Beginn des Förderbands gelegt. Der Inhalt der Packung wird
+# durch einen String definiert.
 def place_item(conveyor, item):
+    # Der erste Speicherplatz des Förderbands (Index 0) wird als Anfang des Förderbandes definiert
     conveyor[0] = item
 
 # Variable für das Förderband mit zehn Einheiten. Der Einheitentyp wird als String gespeichert. Leere Förderplätze
