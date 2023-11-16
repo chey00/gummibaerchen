c25940f09d458575908a00ae119f5a98fbdb33f0
Christian Hey <c.hey@sbs-herzogenaurach.de>
2023-11-16 12:25:17 +0100

    Solved first task.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+def place_item(conveyor, item):
+    conveyor[0] = item
+
 # Variable für das Förderband mit zehn Einheiten. Der Einheitentyp wird als String gespeichert. Leere Förderplätze
 # werden über einen leeren String gekennzeichnet. Zu Begin ist das ganze Förderband leer.
 conveyor_band = [""] * 10
